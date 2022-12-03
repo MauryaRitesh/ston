c5fa246ac2ef942ef953f7a778c846fc0753753e
MauryaRitesh <ritesh.k.m786@gmail.com>
2022-12-03 21:10:57 +0530

    initial

---
+++ account/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name= 'index'),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
+    path('adminpage/', views.admin, name='adminpage'),
+    path('customer/', views.customer, name='customer'),
+    path('employee/', views.employee, name='employee'),
+]
